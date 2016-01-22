01a963cc2eb59be87a9e6c425b6c43a52f2d837c
William Favorite <wfavorite@tablespace.net>
2016-01-22 16:41:57 -0500

    Create support.h
    Like support.c, this will be overwritten.

---
+++ support.h
@@ -0,0 +1,9 @@
+#ifndef SUPPORT_H
+#define SUPPORT_H
+
+/* ========================================================================= */
+__inline__ void eat_ws(char **shand);
+
+char *mkstring(char *cpin);
+
+#endif
