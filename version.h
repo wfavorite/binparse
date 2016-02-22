8861ccc46c03b2ef40e832666658d337198ef02a
William Favorite <wfavorite2@bloomberg.net>
2016-02-22 11:13:18 -0500

    Added testcase for setopt x

--- version.h
+++ version.h
@@ -113,8 +113,9 @@
                      - Minor fixes
                      - Working with data types
                      - grep $TUB * | wc -l ----> 2
+    0.20.0   2/22/16 - Testing of some of the recent features.
 */
-#define VERSION_STRING "0.19.0"
+#define VERSION_STRING "0.20.0"
 /*
   Notes:
 
@@ -123,7 +124,6 @@
        I have marked them with "T" for test.
    [ ] Write check to insure that if a mask= is used, that the data type
        is compatible.
-   [T] "setopt X true" does not work. (Fixed. Not tested.)
    [T] Check where the mask= value is set. (The number is converted from a
        signed value to a unsigned. This should likely always be unsigned.
        or converted to an unsigned without dropping the sign bit.
@@ -168,6 +168,7 @@
    [C] Zeroth (pre-compile) pass should print options set when in verbose mode.
 
   Done:
+   [X] "setopt X true" does not work. (Fixed. Not tested.)
    [X] The -e option is not documented in the -h output.
    [X] Write endian_is() function. Put results in verbose output.
    [X] Apply the mask= in bpdata.c. (Search for mask=, there is a $TUB.)
